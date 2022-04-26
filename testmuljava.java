d42950b6f16a3a3cf853cd84871ac859c9557af7
GauravSajnani <gauravsajnani2000@gmail.com>
2022-04-26 17:05:19 +0530

    Committing class and javfiles

---
+++ testmuljava.java
@@ -0,0 +1,11 @@
+public class testmuljava {
+   public static void main(String[] args) {
+       int a;
+       int b;
+       int c;
+       a = 5;
+       b = 58;
+       c = a*b; //integer number to keep the result of multiplication
+       System.out.println("The multiplication of 2 numbers 5 and 58 is: " + c);
+   }
+}
